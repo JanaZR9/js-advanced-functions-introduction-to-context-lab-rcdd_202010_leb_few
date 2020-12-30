dd68349f630f4bed25e68d3abc6efa7e6cc66552
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-30 14:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ return arr;
 
 //3
 function createTimeInEvent(record,time){
-  let object={};
-    object.type="TimeIn";
-    object.hour=parseInt(time.substring(11));
-    object.date=time.substring(0,10);
-  record.timeInEvents.push(object);
+  let rec={};
+    rec.type="TimeIn";
+    rec.hour=parseInt(time.substring(11));
+    rec.date=time.substring(0,10);
+  record.timeInEvents.push(rec);
   return record;
 }
 
+//4
 function createTimeOutEvent(record,time){
     let object={};
     object.type="TimeOut";
@@ -38,6 +39,7 @@ function createTimeOutEvent(record,time){
   return record;
 }
 
+//5
 function hoursWorkedOnDate (record, time){
     let inDate = record.timeInEvents.find(function(i){
         return i.date === time;
@@ -48,14 +50,20 @@ function hoursWorkedOnDate (record, time){
     return (outDate.hour - inDate.hour) / 100;
 }
 
-  function findEmployeeByFirstName(array,name){
-  let result = array.find((element) => element.firstName===name);
-  return result?result:undefined;
-  }
-
+//6
   function wagesEarnedOnDate(record,time){
   return parseInt(record.payPerHour)*hoursWorkedOnDate(record,time);
   }
+  function findEmployeeByFirstName(array2,name){
+  let result = array2.find((element) => element.firstName===name);
+  if(result)
+  return result;
+  else
+  undefined;
+  }
+
+//7
+ 
 
   function allWagesFor (record){
     let dates = record.timeInEvents.map(function(i){
